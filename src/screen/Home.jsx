48ad51394aea0fddbbacddc1d15e4f12411f372c
kkasg38-stack <kkasg38@gmail.com>
2026-08-24 23:40:49 +0300

    Update Home.jsx

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import io from "socket.io-client";
 
 // export const serverRoute = "http://localhost:8080";
-export const serverRoute = "https://tawtheq-se.onrender.com";
+export const serverRoute = "https://taw-se-production.up.railway.app";
 export const socket = io(serverRoute);
 
 const Home = () => {
